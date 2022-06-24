e1b2752f93d53d6b3368542f08912928bd03a76b
KseniyaSaltik <irbis.mr@gmail.com>
2022-06-24 12:00:36 +0300

    Task#2 complite

--- tasks_1.py
+++ tasks_1.py
@@ -1,15 +1,16 @@
-# Задача №1.
-# Написать метод domain_name, который вернет домен из url адреса.
+'''Задача №1.
+Написать метод domain_name, который вернет домен из url адреса.'''
+
 from urllib.parse import urlparse
 
 
-url1 = "http://github.com/carbonfive/raygun"
-url2 = "http://www.zombie-bites.com"         
-url3 = "https://www.cnet.com" 
-url4 = "www.xakep.ru"
+url1: str = "http://github.com/carbonfive/raygun"
+url2: str = "http://www.zombie-bites.com"         
+url3: str = "https://www.cnet.com" 
+url4: str = "www.xakep.ru"
 
 
-def domain_name(url):
+def domain_name(url: str) -> str:
     if "http" in url or "https" in url:
         result = urlparse(url).netloc
         if url.count('.') > 1:
@@ -23,3 +24,24 @@ def domain_name(url):
         return dom_name
 
 print(domain_name(url3))
+
+
+''' Задача №2. 
+Написать метод int32_to_ip, который принимает на вход 32-битное целое число
+(integer) и возвращает строковое представление его в виде IPv4-адреса.'''
+
+
+num1: int = 2154959208
+num2 = 32
+num3 = 0
+int32: str = "{:032b}".format(num1)
+
+
+def int32_to_ip(int32: str) -> str:
+    ip_addr = [str(int(int32[i:i+8], 2)) for i in range(0,32,8)]
+    result = '.'.join(ip_addr)
+    return result
+
+print(int32_to_ip(int32))
+
+
